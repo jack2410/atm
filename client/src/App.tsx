4ccf33d7b544b2601aae5ae2cfeb88007cd2bfe8
jack2410 <jack.brightsoft@gmail.com>
2021-09-16 20:27:52 +0700

    add and remove atm

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Button } from 'reactstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -9,16 +9,25 @@ import Queue from './components/Queue';
 import useAtm from './hooks/useAtm';
 
 const App = () => {
-  const { atms, queue } = useAtm();
+  const { atms, queue, addData, removeData } = useAtm();
 
   return (
-    <div className="min-vh-100 d-flex flex-column align-items-center justify-content-center">
-      <Row>
-        <Col md={8} className="d-flex align-items-start flex-wrap mb-3">
+    <div className="vh-100 pt-5 d-flex flex-column align-items-center">
+      <Row className="wrapper">
+        <Col md={12} className="mb-2">
           <Row>
+            <Col md={3}>
+              <Button color="primary" size="sm" onClick={addData}>
+                Add new ATM
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+        <Col md={8} className="d-flex align-items-start flex-wrap mb-3 atms">
+          <Row className="w-100">
             {atms.map((atm) => (
-              <Col md={4} key={atm.id}>
-                <ATM atm={atm} />
+              <Col md={4} key={atm.id} className="mb-3">
+                <ATM atm={atm} removeAtm={removeData} />
               </Col>
             ))}
           </Row>
--- client/src/components/ATM.tsx
+++ client/src/components/ATM.tsx
@@ -3,19 +3,29 @@ import { Card, CardBody, CardFooter } from 'reactstrap';
 
 import atmImage from '../assets/images/atm.png';
 import manImage from '../assets/images/man.png';
+import cancelImage from '../assets/images/cancel.png';
 import { ATM as ATMType, ATMStatus } from '../types/ATM';
 
 interface Props {
   atm: ATMType;
+  removeAtm: Function;
 }
 
-const ATM: FC<Props> = ({ atm }: Props) => {
-  const { status, queue } = atm;
+const ATM: FC<Props> = ({ atm, removeAtm }: Props) => {
+  const { id, status, queue } = atm;
   const isBusy = status === ATMStatus.Busy;
 
   return (
     <Card className="d-flex flex-column align-items-center">
       <CardBody className="d-flex flex-column align-items-center">
+        <div className="w-100 d-flex justify-content-end mb-2">
+          <img
+            className="cancel-image cursor-pointer"
+            src={cancelImage}
+            alt="cancelBtn"
+            onClick={() => removeAtm(id)}
+          />
+        </div>
         <img className="atm-image" src={atmImage} alt="atm-machine" />
         <p
           className={`fw-bold fs-2 ${isBusy ? 'text-danger' : 'text-success'}`}
